4a2f8ef11ca00b1e13b21063c80f333ddd5bdb7a
Abhimanue Rajesh <abhimanuemvk@gmail.com>
2026-01-07 23:00:04 +0400

    updated the urls

--- public_interface/urls.py
+++ public_interface/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 
-from django.views.generic import RedirectView
-
+from dashboard.views import dashboard
 from public_interface import views
 
+# from django.views.generic import RedirectView
+
+
 urlpatterns = [
-    path(
-        "",
-        RedirectView.as_view(url="https://www.thedeepseafood.com", permanent=False),
-        name="home-redirect",
-    ),
+    # path(
+    #     "",
+    #     RedirectView.as_view(url="https://www.thedeepseafood.com", permanent=False),
+    #     name="home-redirect",
+    # ),
+    path("", dashboard, name="dashboard"),
     path("visitorsfeedback/", views.visitor_feedback, name="visitorsfeedback"),
 ]
