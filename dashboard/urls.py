2a3e108043d9bb6370436a8d26c6cc14b3f31509
Abhimanue Rajesh <abhimanuemvk@gmail.com>
2026-01-21 21:46:37 +0400

    added the form for oceano and dashboard edits

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,15 @@ from dashboard import views
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("login/", views.user_login, name="user_login"),
+    path("feedback_list/", views.feedback_list, name="feedback_list"),
+    path(
+        "user_data_collection_list/",
+        views.user_data_collection_list,
+        name="user_data_collection_list",
+    ),
+    path(
+        "oceano_spinner_draw_list/",
+        views.oceano_spinner_draw_list,
+        name="oceano_spinner_draw_list",
+    ),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,17 +1,13 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
-from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from public_interface.models import VisitorFeedback
+from django.shortcuts import redirect, render
 
-
-@login_required(login_url="login")
-def dashboard(request):
-    feedback_list = VisitorFeedback.objects.select_related('purpose').order_by('-created_at')
-    context = {
-        'feedback_list': feedback_list
-    }
-    return render(request, "dashboard/dashboard.html", context)
+from public_interface.models import (
+    VisitorFeedback,
+    OceanoSpinnerDraw,
+    CustomerDataCollection,
+)
 
 
 def user_login(request):
@@ -26,3 +22,37 @@ def user_login(request):
     else:
         context = {}
     return render(request, "dashboard/login.html", context)
+
+
+@login_required(login_url="login")
+def dashboard(request):
+    feedback_list = VisitorFeedback.objects.select_related("purpose").order_by(
+        "-created_at"
+    )
+    context = {"feedback_list": feedback_list}
+    return render(request, "dashboard/dashboard.html", context)
+
+
+@login_required(login_url="login")
+def feedback_list(request):
+    feedback_list = VisitorFeedback.objects.select_related("purpose").order_by(
+        "-created_at"
+    )
+    print(feedback_list)
+    context = {"feedback_list": feedback_list}
+    return render(request, "dashboard/feedback_list.html", context)
+
+
+@login_required(login_url="login")
+def user_data_collection_list(request):
+    user_data_list = CustomerDataCollection.objects.order_by("-created_at")
+    context = {"user_data_list": user_data_list}
+    return render(request, "dashboard/user_data_collection.html", context)
+
+
+@login_required(login_url="login")
+def oceano_spinner_draw_list(request):
+    oceano_spinner_data_list = OceanoSpinnerDraw.objects.order_by("-created_at")
+    print(oceano_spinner_data_list)
+    context = {"oceano_spinner_data_list": oceano_spinner_data_list}
+    return render(request, "dashboard/oceano_spinner_data.html", context)
